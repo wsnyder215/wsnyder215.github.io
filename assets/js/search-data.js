e9b8b9ccd94b90c43e453bfd44933e37b3ca0e37
wsnyder215 <wyatt.snyder@hope.edu>
2026-05-17 12:36:46 +0000

    Deploying to gh-pages from @ wsnyder215/wsnyder215.github.io@6637f878081457c6168f0db6ec29ae70a2640b5a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,21 +511,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/CV2026.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%77%79%61%74%74.%73%6E%79%64%65%72@%68%6F%70%65.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
@@ -535,18 +528,11 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/wyatt-snyder-25b4a8315/", "_blank");
         },
       },{
       id: 'light-theme',
